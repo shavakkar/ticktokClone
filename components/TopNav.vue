8a30404c3ff2dcf827ba28149a9339f1ea47e7ef
shavakkar <shavakkar@gmail.com>
2024-02-07 09:23:42 +0530

    stopped till topNav

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -17,13 +17,49 @@ const route = useRoute();
       </div>
 
       <div
-        class="hidden md:flex items-center bg-[#f1f1f2] p-1 rounded-full max-w-[380px w-full]"
+        class="hidden md:flex items-center bg-[#f1f1f2] p-1 rounded-full max-w-[380px] w-full"
       >
         <input
           type="text"
           class="w-full pl-3 py-2 bg-transparent placeholder-[#838383] text-[15px] focus:outline-none"
           placeholder="Search Account"
         />
+        <div class="px-3 py-1 flex items-center border-l border-l-gray-300">
+          <Icon name="ri:search-line" color="#a1a2a7" size="22" />
+        </div>
+      </div>
+
+      <div class="flex items-center justify-end gap-3 min-w-[320px] w-full">
+        <button
+          class="flex items-center border rounded-sm px-3 py-[6px] hover:bg-gray-100"
+        >
+          <Icon name="mdi:plus" color="#000000" size="22" />
+          <span class="px-2 font-medium text-[15px]">Upload</span>
+        </button>
+
+        <div v-if="false" class="flex items-center">
+          <button
+            class="flex items-center bg-[#f02c56] text-white border rounded-md px-3 py-[6px]"
+          >
+            <span class="mx-4 font-medium text-[15px]">Log in</span>
+          </button>
+          <Icon name=" mdi:dots-vertical" color="#161724" size="25" />
+        </div>
+
+        <div class="flex items-center">
+          <Icon
+            class="ml-1 mr-4"
+            name="carbon:send-alt"
+            color="#161724"
+            size="30"
+          />
+          <Icon
+            class="mr-5"
+            name="bx:message-detail"
+            color="#161724"
+            size="27"
+          />
+        </div>
       </div>
     </div>
   </div>
