1aa9e10b5bc5414fba65f74d07068e297c29695d
shavakkar <shavakkar@gmail.com>
2024-02-07 08:24:46 +0530

    resloved NuxtLayout warning

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -15,6 +15,16 @@ const route = useRoute();
           <img width="115" src="~/assets/images/tiktok-logo.png" />
         </NuxtLink>
       </div>
+
+      <div
+        class="hidden md:flex items-center bg-[#f1f1f2] p-1 rounded-full max-w-[380px w-full]"
+      >
+        <input
+          type="text"
+          class="w-full pl-3 py-2 bg-transparent placeholder-[#838383] text-[15px] focus:outline-none"
+          placeholder="Search Account"
+        />
+      </div>
     </div>
   </div>
 </template>
