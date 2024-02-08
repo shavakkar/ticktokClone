593d28f74ae4f3729b5c0001473833d6aaebf22c
shavakkar <shavakkar@gmail.com>
2024-02-08 22:53:10 +0530

    created laravelApi dir

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default defineNuxtPlugin((NuxtApp) => {
     
     axios.defaults.withCredentials = true;
-    axios.defaults.baseURL = 'http://localhost:3000';
+    axios.defaults.baseURL = 'http://localhost:8000';
     
     return {
         provide:{
