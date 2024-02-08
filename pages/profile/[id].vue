ec6c7a233273e6ff8aa8f2f68b6f60c1546e6006
shavakkar <shavakkar@gmail.com>
2024-02-08 14:05:43 +0530

    done w profile

--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -12,6 +12,7 @@
           <div class="text-[30px] font-bold truncate">User Name</div>
           <div class="text-[18px] truncate">User Name</div>
           <button
+            v-if="true"
             class="flex items-center rounded-md py-1.5 px-3.5 mt-3 text-[15px] font-semibold border hover:bg-gray-100"
           >
             <Icon class="mt-0.5 mr-1" name="mdi:pencil" size="18" />
@@ -19,11 +20,60 @@
           </button>
 
           <button
-            class="flex items-center rounded-md my-1.5 px-8 mt-3 text-[15px] text-white font-semibold bg-[#f02c56]"
+            v-else
+            class="flex items-center rounded-md py-1.5 px-8 mt-3 text-[15px] text-white font-semibold bg-[#f02c56]"
           >
             Follow
           </button>
         </div>
+      </div>
+
+      <div class="flex items-center pt-4">
+        <div class="mr-4">
+          <span class="font-bold"> 10k </span>
+          <span class="text-gray-500 font-light text-[15px] pl-1.5"
+            >Following</span
+          >
+        </div>
+        <div class="mr-4">
+          <span class="font-bold"> 44 </span>
+          <span class="text-gray-500 font-light text-[15px] pl-1.5"
+            >Followers</span
+          >
+        </div>
+        <div class="mr-4">
+          <span class="font-bold"> 3k </span>
+          <span class="text-gray-500 font-light text-[15px] pl-1.5">Likes</span>
+        </div>
+      </div>
+
+      <div
+        class="pt-4 mr-4 text-gray-500 font-light text-[15px] pl-1.5 max-w-[500px]"
+      >
+        This is the bio section
+      </div>
+
+      <div class="w-full flex items-center pt-4 border-b">
+        <div
+          class="w-60 text-center py-2 text-[17px] font-semibold border-b-2 border-b-black"
+        >
+          Videos
+        </div>
+        <div
+          class="w-60 text-gray-500 text-center py-2 text-[17px] font-semibold"
+        >
+          <Icon name="material-symbols:lock-open" class="mb-0.5" />
+          Liked
+        </div>
+      </div>
+
+      <div
+        class="mt-4 grid 2xl:grid-cols-6 xl:grid-cols5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3"
+      >
+        <PostUser /> <PostUser /> <PostUser /> <PostUser /> <PostUser />
+        <PostUser /> <PostUser /> <PostUser /> <PostUser /> <PostUser />
+        <PostUser /> <PostUser /> <PostUser /> <PostUser /> <PostUser />
+        <PostUser /> <PostUser /> <PostUser />
       </div>
     </div>
   </MainLayout>
@@ -31,4 +81,5 @@
 
 <script setup>
 import MainLayout from "~/layouts/MainLayout.vue";
+import PostUser from "~/components/PostUser.vue";
 </script>
