4c9ab5bfda4bed568e3fae6f5268deb0b28831d7
shavakkar <shavakkar@gmail.com>
2024-02-11 16:05:55 +0530

    added few changes

--- app.vue
+++ app.vue
@@ -10,6 +10,30 @@
 
 <script setup>
 import { storeToRefs } from "pinia";
-const { $generalStore } = useNuxtApp();
+const { $userStore, $generalStore } = useNuxtApp();
 const { isLoginOpen, isEditProfileOpen } = storeToRefs($generalStore);
+
+onMounted(async () => {
+  $generalStore.bodySwitch(false);
+  isLoginOpen.value = false;
+  isEditProfileOpen.value = false;
+
+  try {
+    await $generalStore.hasSessionExpired();
+    if ($userStore.id) {
+      $userStore.getUser();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+watch(
+  () => isLoginOpen.value,
+  (val) => $generalStore.bodySwitch(val)
+);
+watch(
+  () => isEditProfileOpen.value,
+  (val) => $generalStore.bodySwitch(val)
+);
 </script>
--- stores/general.js
+++ stores/general.js
@@ -1,4 +1,8 @@
 import { defineStore } from "pinia";
+import { useUserStore } from "./user";
+import axios from "~/plugins/axios";
+
+const $axios = axios().provide.axios;
 
 export const useGeneralStore = defineStore('general',{
     state: () => ({ 
@@ -12,7 +16,35 @@ export const useGeneralStore = defineStore('general',{
         following: null,
      }),
     actions: {
-        // 
+        bodySwitch(val){
+            if(val){
+                document.body.style.overflow = 'hidden';
+                return;
+            } 
+            document.body.style.overflow = 'visible';
+        },
+
+        async hasSessionExpired(){
+            await $axios.interceptors.response.use((response) =>{
+                return response;
+            }, (error) => {
+                switch (error.response.status){
+                    case 401:  // Not logged in 
+                    case 419:  // Session expired
+                    case 503:  // Down for maintenance
+                        //Bounce the user to the login screen with a redirect bacl
+                        useUserStore().resetUser();
+                        window.location.href = '/';
+                        break;
+                    case 500:
+                        alert('Oops, something went wrong! the team has been notified.')
+                        break;
+                    default:
+                        // Allow individual requests to handle other errors
+                        return Promise.reject(error);
+                }
+            })
+        }
     },
     persist: true,
 })
